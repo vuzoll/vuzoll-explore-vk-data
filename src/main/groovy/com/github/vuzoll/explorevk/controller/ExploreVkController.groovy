33baa82161bde8076987d3e47e8ccf7ca4bd399b
Yaroslav Yermilov <yermilov17@gmail.com>
2017-01-16 16:39:55 +0200

    #31. explore size of 'vk-ingested' data

--- src/main/groovy/com/github/vuzoll/explorevk/controller/ExploreVkController.groovy
+++ src/main/groovy/com/github/vuzoll/explorevk/controller/ExploreVkController.groovy
@@ -1,9 +1,22 @@
 package com.github.vuzoll.explorevk.controller
 
+import com.github.vuzoll.explorevk.services.ExploreVkService
+import com.github.vuzoll.explorevk.services.VkExploration
 import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @Slf4j
 class ExploreVkController {
+
+    @Autowired
+    ExploreVkService exploreVkService
+
+    @PostMapping(path = '/explore')
+    @ResponseBody VkExploration explore() {
+        log.info 'Receive exploration request'
+
+        exploreVkService.explore()
+    }
 }
---
+++ src/main/groovy/com/github/vuzoll/explorevk/services/VkExploration.groovy
@@ -0,0 +1,6 @@
+package com.github.vuzoll.explorevk.services
+
+class VkExploration {
+
+    Integer datasetSize
+}
